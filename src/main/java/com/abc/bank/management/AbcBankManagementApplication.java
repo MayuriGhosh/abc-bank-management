21f5b9a8571d7f65798489beb74944c0eb568dfb
05463O744 <05463O744@IBM-PF3F1KDT.in.ibm.com>
2023-03-23 16:27:58 +0530

    Initial Commit

---
+++ src/main/java/com/abc/bank/management/AbcBankManagementApplication.java
@@ -0,0 +1,13 @@
+package com.abc.bank.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBankManagementApplication.class, args);
+	}
+
+}
